7b148e8b62eba97a698ce7df58155eda3c1d595c
akash <akash.ry@somaiya.edu>
2021-06-30 22:29:59 +0530

    Single command "npm run dev" added for startng both client and server

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 const db = require('./controllers/dbm');
-const manpower=require('./routes/hiredmanpower');
-const cors=require('cors');
+const manpower = require('./routes/hiredmanpower');
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,7 +13,7 @@ app.use(cors({
 	credentials: true
 }))
 
-app.use('/manpower',manpower);
+app.use('/manpower', manpower);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log("Server started ");

0e366178e0e660afe4da1d8b8cc013b9031a40ec
akash <akash.ry@somaiya.edu>
2021-07-15 22:26:08 +0530

    Create Vendor done

--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -1,24 +1,24 @@
 const vendor = require('../models/vendor.model');
 
-const router=require('express').Router();
+const router = require('express').Router();
 
-router.post('/',async (req,res)=>{
-    var { vendor_name, vendor_id, invoice, region}= req.body;
-    if(typeof vendor_name==='undefined' || typeof vendor_id==='undefined' || typeof invoice==='undefined' || typeof region==='undefined'
-      ||  vendor_id==='' || vendor_name==='' || invoice==='' || region===''){
-       return res.status(403).send({"error":"please provide all the information"});
+router.post('/', async (req, res) => {
+    var { vendor_name, invoice, region } = req.body;
+    if (typeof vendor_name === 'undefined' || typeof invoice === 'undefined' || typeof region === 'undefined'
+        || vendor_name === '' || region === '') {
+        return res.status(403).send({ "error": "please provide all the information" });
     }
     try {
-        var new_vendor= vendor()
-        new_vendor.vendor_name=vendor_name.trim();
-        new_vendor.vendor_id=vendor_id.trim();
-        new_vendor.invoice=invoice.trim();
-        new_vendor.region=region.trim();
-        let result= await new_vendor.save();
+        var new_vendor = vendor()
+        new_vendor.vendor_name = vendor_name.trim();
+        // new_vendor.vendor_id = vendor_id.trim();
+        new_vendor.invoice = [];
+        new_vendor.region = region.trim();
+        let result = await new_vendor.save();
         res.status(200).send(result);
     } catch (error) {
         res.status(500).send();
     }
 });
 
-module.exports=router;
+module.exports = router;

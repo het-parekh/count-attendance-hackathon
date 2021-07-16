e453e94396409d0b0a7d066997cb03d296bffed1
dakshit050 <dakshit.v@somaiya.edu>
2021-07-16 15:59:16 +0530

    Bill added

---
+++ server/models/bill.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const bill_Schema= new mongoose.Schema({
+        Vendor_ref:{type:mongoose.Types.ObjectId, ref:'vendor'},
+    	invoice:{type:mongoose.Types.ObjectId, ref:'invoice'},
+    	number_of_employees:[{
+            Name:{type:String},
+            amount:{type:Number}
+        }],
+    	service_month:{type:String},	
+    	base_cost:{type:Number},		
+    	extra_charges:{type:Number},	
+    	total_cost:{type:Number}
+            
+});
+
+const bill = mongoose.model('bill',bill_Schema);
+module.exports  = bill;
--- server/models/vendor.model.js
+++ server/models/vendor.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const vendor_Schema= new mongoose.Schema({
         vendor_name:{type:String},	
     	region:{type:String},
-		sla:{gunman:{type:String},driver:{type:String},vehicle:{type:String}}		
+		sla:{gunman:{type:String},driver:{type:String},vehicle:{type:String}},
+		sla_ot:{gunman:{type:String},driver:{type:String},vehicle:{type:String}}		
 });
 
 const vendor = mongoose.model('vendor',vendor_Schema);

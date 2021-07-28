3b2253b4776421b0cb50dcbc78b709a481301892
Het <het.parekh@somaiya.edu>
2021-07-28 18:34:06 +0530

    app.get(*) added

--- server/app.js
+++ server/app.js
@@ -34,6 +34,11 @@ const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV == 'production'){
 	app.use(express.static('frontend/build'))
+
+	const path = require('path');
+  	app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  	});
 }
 app.listen(PORT, () => {
 	console.log("Server started ");
